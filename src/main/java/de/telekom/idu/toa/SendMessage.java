c8f8b81232853698566f97ae5b541cbb0d0dbba7
Sebastian Lang <sebastian-lang@telekom.de>
2018-07-27 15:17:32 +0200

    fix

--- src/main/java/de/telekom/idu/toa/SendMessage.java
+++ src/main/java/de/telekom/idu/toa/SendMessage.java
@@ -1,10 +1,19 @@
 package de.telekom.idu.toa;
 
-import com.github.seratch.jslack.*;
-import com.github.seratch.jslack.api.webhook.*;
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.webhook.Payload;
+import com.github.seratch.jslack.api.webhook.WebhookResponse;
+import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Message;
+import io.elastic.api.Module;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
-public class SendMessage implements io.elastic.api.Module {
+public class SendMessage implements Module {
+
+    private static final Logger logger = LoggerFactory.getLogger(SendMessage.class);
 
     public static void main(String[] args) throws IOException {
 
@@ -13,7 +22,15 @@ public class SendMessage implements io.elastic.api.Module {
         System.setProperty("https.proxyHost", "HE103024.emea1.cds.t-internal.com");
         System.setProperty("https.proxyPort", "8080");
 
-        String url = " https://hooks.slack.com/services/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"; //System.getenv("SLACK_WEBHOOK_URL");
+    }
+
+    @Override
+    public void execute(ExecutionParameters parameters) {
+
+        logger.info("About to send a message to Slack");
+
+        String url = parameters.getConfiguration().getString("channel");
+        //" https://hooks.slack.com/services/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"; //System.getenv("SLACK_WEBHOOK_URL");
 
         Payload payload = Payload.builder()
                 .channel("#allgemein")
@@ -23,7 +40,18 @@ public class SendMessage implements io.elastic.api.Module {
                 .build();
 
         Slack slack = Slack.getInstance();
-        WebhookResponse response = slack.send(url, payload);
+        WebhookResponse response = null;
+        try {
+            response = slack.send(url, payload);
+        } catch (IOException e) {
+            e.printStackTrace();
+            parameters.getEventEmitter().emitException(e);
+        }
 
+        logger.info("Response Code: " + response.getCode());
+
+        Message message = parameters.getMessage();
+
+        parameters.getEventEmitter().emitData(message);
     }
 }
